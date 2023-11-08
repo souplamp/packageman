dbe63f19127b163c0e317b7138cee93c4ca90a50
souplamp <camden.mcall@gmail.com>
2023-11-08 13:25:19 -0600

    IT WOOOORKS ITS ALLLIVE

--- mansion/maze.gd
+++ mansion/maze.gd
@@ -4,7 +4,8 @@ extends TileMap
 
 var current_head: Vector2i = Vector2i(6, 5)
 
-var last_dir: Vector2i
+var length: int = 5
+var tiles: Array[Vector2i] = [current_head]
 
 var dir: Array[Vector2i] = [Vector2i.UP, Vector2i.DOWN, Vector2i.RIGHT, Vector2i.LEFT]
 enum DIR { UP, DOWN, RIGHT, LEFT }
@@ -14,54 +15,26 @@ func _ready() -> void:
 	set_cell(1, current_head, 1, Vector2i.ZERO)
 
 func move() -> void:
-	var next_head: Vector2i = current_head + dir.pick_random()
-	var ctd: TileData = get_cell_tile_data(0, next_head)
-	var std: TileData = get_cell_tile_data(1, next_head)
-	if ctd != null or std != null: move()
-	else:
-		set_cell(1, next_head, 1, Vector2i.ZERO)
-		current_head = next_head
-
-#func move() -> void:
-#	var invalid: bool = true
-#	var next_head: Vector2i
-#	var random_dir: Vector2i
-#
-#	while invalid:
-#
-#		random_dir = dir.pick_random()
-#		next_head = current_head + random_dir
-#
-#		var ctd: TileData = get_cell_tile_data(0, next_head)
-#		#var std: TileData = get_cell_tile_data(1, next_head)
-#
-#		invalid = ctd != null
-#		await get_tree().process_frame
-#
-#	last_dir = random_dir
-#
-#	set_cell(1, next_head, 1, Vector2i.ZERO)
-#	current_head = next_head
-
-func verify_tile(next_position: Vector2i) -> bool:
-	var b: bool = false
-	
-	
-	
-	return b
-
-func pick_direction() -> Vector2i:
+	print(tiles)
 	var invalid: bool = true
-	var picked_dir: Vector2i
+	var next_head: Vector2i = current_head + dir.pick_random()
 	
 	while invalid:
-		picked_dir = dir.pick_random()
-		var difference = last_dir - picked_dir
+		next_head = current_head + dir.pick_random()
+		var ctd: TileData = get_cell_tile_data(0, next_head)
+		var std: TileData = get_cell_tile_data(1, next_head)
 		
-		invalid = difference == Vector2i.ZERO
+		invalid = ctd != null or std != null
 		await get_tree().process_frame
 	
-	return picked_dir
-
+	current_head = next_head
+	
+	if len(tiles) > length:
+		erase_cell(1, tiles[0])
+		tiles.remove_at(0)
+	tiles.append(current_head)
+	set_cell(1, current_head, 1, Vector2i.ZERO)
+	
 func _on_move_timeout():
+	print("timed out")
 	move()
